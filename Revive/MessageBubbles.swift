ae6b4c7d5fb35726327232b3887009a7f52cac37
Melanie-ai <ma.melanielaveriano@gmail.com>
2025-03-27 13:07:14 -0400

    Test

---
+++ Revive/MessageBubbles.swift
@@ -0,0 +1,18 @@
+//
+//  MessageBubbles.swift
+//  Revive
+//
+//  Created by Melanie Laveriano on 3/20/25.
+//
+
+import SwiftUI
+
+struct MessageBubbles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageBubbles()
+}

5cc40909a1e52ad01bc21ab1a05b51b2201e7455
Melanie-ai <ma.melanielaveriano@gmail.com>
2025-03-27 13:09:22 -0400

    Test

--- Revive/SwiftUIView.swift
+++ Revive/SwiftUIView.swift
@@ -5,14 +5,7 @@
 //  Created by Melanie Laveriano on 3/26/25.
 //
 
-import SwiftUI
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    SwiftUIView()
-}
